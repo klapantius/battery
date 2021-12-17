1a1d0a8ad776432e645bb34cc652fceb7e2f6944
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2021-12-17 15:53:45 +0100

    switch to google drive based file trigger

--- onoff.ps1
+++ onoff.ps1
@@ -1,10 +1,20 @@
-$lowerTreshold = 22
-$upperTreshold = 82
-
-$proc =  Get-WmiObject Win32_Battery | select -ExpandProperty EstimatedChargeRemaining
-if (($proc -lt $lowerTreshold) -or ($upperTreshold -lt $proc)) {
+function commit_a_change {
   $message = "$(Get-Date) - $proc%"
   $message | Out-File onoff.txt -Append
   git commit -am "$message"
   git push
+}
+
+function place_a_trigger_file {
+  $path='g:\My Drive\iktato\laptop'
+  del $path\\*
+  "switch" | out-file "$path\$(get-date -Format yyMMdd_HHmm)_$proc"
+}
+
+$lowerTreshold = 65 #22
+$upperTreshold = 70 #82
+
+$proc =  Get-WmiObject Win32_Battery | select -ExpandProperty EstimatedChargeRemaining
+if (($proc -lt $lowerTreshold) -or ($upperTreshold -lt $proc)) {
+  place_a_trigger_file
 }
--- switch.ps1
+++ switch.ps1
@@ -1,3 +1,7 @@
-Get-Date | Out-File onoff.txt
-git commit -am "switch"
-git push
+#Get-Date | Out-File onoff.txt
+#git commit -am "switch"
+#git push
+
+$path='g:\My Drive\iktato\laptop'
+"x" | out-file "$path\$(get-date -Format yyMMdd_HHmm)_manual"
+
