e67566ddff0fffc6b6ba2e47c863588702fe98c3
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2021-12-19 21:11:48 +0100

    cover simple limit evaluation with unit tests

--- onoff.tests.ps1
+++ onoff.tests.ps1
@@ -9,4 +9,18 @@ Describe "evaluation procedure" {
             }
         }
     }
+    Context "between limits" {
+        20, 21, 50, 79, 80 | ForEach-Object {
+            It "results $false (battery level $_%)" {
+                evaluate -proc $_ -lowerTreshold 20 -upperTreshold 80 | Should -be $false
+            }
+        }
+    }
+    Context "treshold exceeded" {
+        19, 81 | ForEach-Object {
+            It "results $true (battery level $_%)" {
+                evaluate -proc $_ -lowerTreshold 20 -upperTreshold 80 | Should -be $true
+            }
+        }
+    }
 }
