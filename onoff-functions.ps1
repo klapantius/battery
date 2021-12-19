91b9c664359bece4b3c89dcb88b8b59f0999f37c
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2021-12-19 20:41:34 +0100

    carve out launching

---
+++ onoff-functions.ps1
@@ -0,0 +1,68 @@
+function commit_a_change {
+  $message = "$(Get-Date) - $proc%"
+  $message | Out-File onoff.txt -Append
+  git commit -am "$message"
+  git push
+}
+
+function place_a_trigger_file {
+  $path = 'g:\My Drive\iktato\laptop'
+  del $path\\*
+  "switch" | out-file "$path\$(get-date -Format yyMMdd_HHmm)_$proc"
+}
+
+function show-notification {
+  [cmdletbinding()]
+  param(
+    [parameter(ValueFromPipeline)]
+    [string]$message
+  )
+  Add-Type -AssemblyName System.Windows.Forms 
+  $global:balloon = New-Object System.Windows.Forms.NotifyIcon
+  $path = (Get-Process -id $pid).Path
+  $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
+  $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info 
+  $balloon.BalloonTipText = $message
+  $balloon.BalloonTipTitle = "battery saver" 
+  $balloon.Visible = $true
+  $balloon.ShowBalloonTip(5000)
+}
+
+function get-batteryLevel {
+  return Get-WmiObject Win32_Battery | select -ExpandProperty EstimatedChargeRemaining
+}
+
+function evaluate {
+  return $force -or ($proc -lt $lowerTreshold) -or ($upperTreshold -lt $proc)
+}
+
+function trigger-ifttt {
+  param(
+    [parameter(ValueFromPipeline)]
+    [switch]$armed
+  )
+  if ($armed) { place_a_trigger_file }
+  return $armed
+}  
+
+function compose-message {
+  param(
+    [int]$proc,
+    [switch]$force
+  )
+  $message = "$proc -> switched"
+  if ($force) { $message = "$message (manual)" }
+  return $message
+}
+
+function launch { 
+  param(
+    [switch]$force,
+    [int]$lowerTreshold = 65, #22
+    [int]$upperTreshold = 80  #82
+  )
+  $proc = get-batteryLevel
+  if (evaluate $proc | trigger-ifttt) {
+    compose-message -proc $proc -force $force | show-notification
+  }
+}
--- onoff.ps1
+++ onoff.ps1
@@ -2,70 +2,9 @@
 param(
   [switch]$force,
   [int]$lowerTreshold = 65, #22
-  [int]$upperTreshold = 80, #82
-  [switch]$testMode
+  [int]$upperTreshold = 80  #82
 )
 
-function commit_a_change {
-  $message = "$(Get-Date) - $proc%"
-  $message | Out-File onoff.txt -Append
-  git commit -am "$message"
-  git push
-}
+Import-Module '.\onoff-functions.ps1' -force
 
-function place_a_trigger_file {
-  $path = 'g:\My Drive\iktato\laptop'
-  del $path\\*
-  "switch" | out-file "$path\$(get-date -Format yyMMdd_HHmm)_$proc"
-}
-
-function show-notification {
-  [cmdletbinding()]
-  param(
-    [parameter(ValueFromPipeline)]
-    [string]$message
-  )
-  Add-Type -AssemblyName System.Windows.Forms 
-  $global:balloon = New-Object System.Windows.Forms.NotifyIcon
-  $path = (Get-Process -id $pid).Path
-  $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
-  $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info 
-  $balloon.BalloonTipText = $message
-  $balloon.BalloonTipTitle = "battery saver" 
-  $balloon.Visible = $true
-  $balloon.ShowBalloonTip(5000)
-}
-
-function get-batteryLevel {
-  return Get-WmiObject Win32_Battery | select -ExpandProperty EstimatedChargeRemaining
-}
-
-function evaluate {
-  return $force -or ($proc -lt $lowerTreshold) -or ($upperTreshold -lt $proc)
-}
-
-function trigger-ifttt {
-  param(
-    [parameter(ValueFromPipeline)]
-    [switch]$armed
-  )
-  if ($armed) { place_a_trigger_file }
-  return $armed
-}  
-
-function compose-message {
-  param(
-    [int]$proc,
-    [switch]$force
-  )
-  $message = "$proc -> switched"
-  if ($force) { $message = "$message (manual)" }
-  return $message
-}
-
-if (!$testMode) { 
-  $proc = get-batteryLevel
-  if (evaluate $proc | trigger-ifttt) {
-    compose-message -proc $proc -force $force | show-notification
-  }
-}
+launch -force $force -lowerTreshold $lowerTreshold -upperTreshold $upperTreshold
