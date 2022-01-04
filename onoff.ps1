c81c267e153bd06bd24dc940423fc5f14d2346e0
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2022-01-04 10:01:27 +0100

    try to work in the background
    learned that background jobs cannot write (wtf!?!?!?) files, thus logging is not possible nor the trigger file can be created

--- onoff.ps1
+++ onoff.ps1
@@ -2,9 +2,43 @@
 param(
   [bool]$force = $false,
   [int]$lowerTreshold = 30, #22
-  [int]$upperTreshold = 80  #82
+  [int]$upperTreshold = 80, #82
+  [int]$iterationDelay = 0,
+  [switch]$terminate
 )
 
 Import-Module '.\onoff-functions.ps1' -force
 
-launch -force $force -lowerTreshold $lowerTreshold -upperTreshold $upperTreshold
+$jobName = "BatteryCharger"
+
+function Start-Once {
+  write-log ">>>>> Start-Once"
+  launch -force $force -lowerTreshold $lowerTreshold -upperTreshold $upperTreshold
+  write-log "----- Start-Once completed"
+}
+
+$job = Get-Job -Name $jobName -ErrorAction SilentlyContinue
+if ($null -ne $job -and ($iterationDelay -gt 0 -or $terminate.IsPresent)) {
+  write-log "stopping earlier job ""$($job.Name) $($job.Id)"""
+  Stop-Job -Name $jobName -ErrorAction SilentlyContinue
+  Remove-Job -Name $jobName -ErrorAction SilentlyContinue
+  if ($terminate.IsPresent) {
+    write-log "script is done, it has just terminated a pending job"
+    return 
+  }
+  write-log "continuing after the clean-up sequence"
+}
+
+if ($iterationDelay -gt 0) {
+  # write-log "start an iterating job"
+  # $thisJob = Start-Job -Name $jobName -Script {
+  #   $jobTitle = "$($thisJob.Name) ($($thisJob.Id))"
+  #   write-log "$jobTitle created"
+  do {
+    write-log "new iteration starts"
+    Start-Once
+    Start-Sleep -Seconds $(60 * $iterationDelay)
+  } while ($true) 
+  # }
+}
+else { Start-Once }
