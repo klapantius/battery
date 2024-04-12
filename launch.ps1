1131dd9ed07c4a7d37a523e1bc0a3a011b35d7f3
Juhasz, Balazs (SHS DI SY R&D REL DS) <balazs.juhasz@siemens-healthineers.com>
2024-04-12 11:41:12 +0200

    bugfix

--- launch.ps1
+++ launch.ps1
@@ -1,7 +1,10 @@
 # Prototype to schedule the battery-charger script as a scheduled task
 # Credits to https://sid-500.com/2022/08/17/how-to-include-your-powershell-7-scripts-in-task-scheduler/
+# !!! but this doesn't work very well !!!
 
-$Action = New-ScheduledTaskAction -Execute "pwsh –Noprofile -WindowStyle Hidden -ExecutionPolicy Bypass -File $PSScriptRoot\onoff.ps1"
+Import-Module -Name ScheduledTasks -UseWindowsPowerShell
+
+$Action = New-ScheduledTaskAction -Execute "pwsh" -Argument "–Noprofile -WindowStyle Hidden -ExecutionPolicy Bypass -File $PSScriptRoot\onoff.ps1"
 $Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5)
 $Principal = New-ScheduledTaskPrincipal -UserId ad005\z001rybj
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Principal
