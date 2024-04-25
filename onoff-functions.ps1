bb0fba0f833cc43d1127762c079b1b7e524fcf73
Juhasz, Balazs (SHS DI SY R&D REL DS) <balazs.juhasz@siemens-healthineers.com>
2024-04-25 07:20:43 +0200

    sync 240425_0720

--- onoff-functions.ps1
+++ onoff-functions.ps1
@@ -58,7 +58,7 @@ function evaluate {
 function trigger-ifttt {
   param(
     [parameter(ValueFromPipeline)]
-    [string]$level
+    $level
   )
   if ($null -ne $level) {
     write-log "triggering with $level%"
